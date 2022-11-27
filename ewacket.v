466709e43c9eb1a2634e22697ced7d15a4b1fdba
Anders Groeschel <andersgroeschel@gmail.com>
2022-11-27 10:22:10 -0500

    added notation to wacket language, mostly sure I did that correctly, and fixed a few small errors with the inductive

--- ewacket.v
+++ ewacket.v
@@ -2,21 +2,41 @@ Require Import String. Local Open Scope string.
 Require Import Arith Bool List ZArith.
 
 Inductive wack: Type :=
-| S_Int Int -> wack
+(* note this needs to be changed to integers, 
+I'm just lazy and haven't figured out the proper 
+way to do this yet*)
+| S_Int: nat -> wack 
 | S_Plus: wack -> wack -> wack
 | S_Minus: wack -> wack -> wack
 | S_Mult: wack -> wack -> wack
 | S_Div: wack -> wack -> wack
 
-| S_Bool bool -> wack
-| S_And wack -> wack -> wack
-| S_Or wack -> wack -> wack
-| S_Not wack -> wack
+| S_Bool: bool -> wack
+| S_And: wack -> wack -> wack
+| S_Or: wack -> wack -> wack
+| S_Not: wack -> wack
 
-| S_IfElse wack -> wack -> wack -> wack 
+| S_IfElse: wack -> wack -> wack -> wack 
 (* maybe function calls or something I don't 
 know everything thats supposed to be in this language*)
 .
+
+(* don't know how to do notation for integers or bools*)
+Notation "x + y" := (S_Plus x y).
+Notation "x - y" := (S_Minus x y).
+Notation "x * y" := (S_Mult x y).
+Notation "x / y" := (S_Div x y).
+
+Notation "a && b" := (S_And a b).
+Notation "a || b" := (S_Or a b).
+Notation "! a" := (S_Not a) (at level 99). (* don't know if that's the right level*)
+
+Notation "'if' a 'then' x 'else' y" := 
+    (S_IfElse a x y) (at level 89,
+            a at level 99,
+            x at level 99,
+            y at level 99).
+
 
 (* this should be taken from an open source project*)
 Inductive wasm : Type :=
