8e1b043446e03899613c55c6f4a8266258c55df1
Anders Groeschel <andersgroeschel@gmail.com>
2022-11-29 13:05:07 -0500

    added wasm stuff to compiler file until we get formalization

--- src/compiler.v
+++ src/compiler.v
@@ -1,11 +1,23 @@
 Require Import wacket.
 Require Import observable.
+Require Import String. Local Open Scope string.
+
+(* this should be taken from an open source project*)
+Inductive wasm : Type :=
+
+.
+
+Reserved Notation " t 'c==>' n " (at level 50, left associativity).
+Inductive wasmEval: wasm -> observable -> Prop :=
+    
+where " t 'c==>' n " := (wasmEval t n).
+
+
 
 Inductive compilerResult : Type :=
 | Succ : wasm -> compilerResult
 | Error : string -> compilerResult
 .
 
-
 (* the compiler from wacket to wasm, if *)
-Fixpoint wasmCompiler (source : wack) : compilerResult := Error "not implemented" .
+Fixpoint wasmCompiler (source : dupe) := Error "not implemented" .
--- src/ewacket.v
+++ src/ewacket.v
@@ -1,41 +1,15 @@
 Require Import Arith Bool List ZArith.
-Require Import String. Local Open Scope string.
 Require Import wacket.
 Require Import observable.
-
-
-(* this should be taken from an open source project*)
-Inductive wasm : Type :=
-
-.
-
-Reserved Notation " t 'c==>' n " (at level 50, left associativity).
-Inductive wasmEval: wasm -> observable -> Prop :=
-    
-where " t 'c==>' n " := (wasmEval t n).
-
-
-Inductive compilerResult : Type :=
-| Succ : wasm -> compilerResult
-| Error : string -> compilerResult
-.
+Require Import compiler.
 
 
 
 
-(* the compiler from wacket to wasm, if *)
-Fixpoint wasmCompiler (source : wack) : compilerResult := Error "not implemented" .
 
 Theorem semanticPreservation:
-    forall (S : wack) (C : wasm) (obs: observable), (wasmCompiler S) = (Succ C) ->
-    S s==> obs -> C c==> obs.
+    forall (S : dupe) (C : wasm) (obs: observable), (wasmCompiler S) = (Succ C) ->
+    (S d==> obs) -> (C c==> obs).
 Proof.
-    
-Qed.
-
-
-
-
-
-
+Admitted.
 
