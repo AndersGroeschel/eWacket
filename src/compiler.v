b82c4800d0f8369704803a1b9936c41650dfd4f1
Anders Groeschel <andersgroeschel@gmail.com>
2022-12-02 15:44:15 -0500

    changed compiler to use wasmLite

--- src/compiler.v
+++ src/compiler.v
@@ -1,21 +1,11 @@
 Require Import wacket.
 Require Import observable.
 Require Import String. Local Open Scope string.
-
-(* this should be taken from an open source project*)
-Inductive wasm : Type :=
-
-.
-
-Reserved Notation " t 'c==>' n " (at level 50, left associativity).
-Inductive wasmEval: wasm -> observable -> Prop :=
-    
-where " t 'c==>' n " := (wasmEval t n).
-
+Require Import wasmLite.
 
 
 Inductive compilerResult : Type :=
-| Succ : wasm -> compilerResult
+| Succ : wasmLite -> compilerResult
 | Error : string -> compilerResult
 .
 
