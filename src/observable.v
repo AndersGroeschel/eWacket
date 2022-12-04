d109cf8b906defb1e75517916495c7e58a039d92
Anders Groeschel <andersgroeschel@gmail.com>
2022-12-04 18:05:04 -0500

    added dupe result type and got rif of observable

--- src/observable.v
+++
@@ -1,9 +0,0 @@
-Require Import Arith Bool List ZArith.
-
-Inductive observable : Type :=
-(* don't know how we want to define this yet, one option 
-is to take printed values, another is to take output*)
-| O_Int: Z -> observable
-| O_Bool: bool -> observable
-| O_Error
-.
--- src/wacket.v
+++ src/wacket.v
@@ -24,29 +24,34 @@ Notation "'(' 'sub1' e ')'" := (D_sub1 e).
 Notation "'(' 'zero?' e ')'" := (D_zero e).
 Notation "'(' 'If' b 'Then' t 'Else' e ')'" := (D_if b t e).
 
+Inductive dupeResult : Type :=
+| DR_Int: Z -> dupeResult
+| DR_Bool: bool -> dupeResult
+| DR_Error: dupeResult
+.
 
 Fixpoint evalDupe(d : dupe) := match d with 
-| (Int z) => (O_Int z)
-| (Bool b) => (O_Bool b)
+| (Int z) => (DR_Int z)
+| (Bool b) => (DR_Bool b)
 | (add1 e) => match (evalDupe e) with 
-    | O_Int i => O_Int (i + 1)
-    | O_Bool b => O_Error
-    | O_Error => O_Error
+    | DR_Int i => DR_Int (i + 1)
+    | DR_Bool b => DR_Error
+    | DR_Error => DR_Error
     end
 | (sub1 e) => match (evalDupe e) with 
-    | O_Int i => O_Int (i - 1)
-    | O_Bool b => O_Error
-    | O_Error => O_Error
+    | DR_Int i => DR_Int (i - 1)
+    | DR_Bool b => DR_Error
+    | DR_Error => DR_Error
     end
 | (zero? e) => match (evalDupe e) with 
-    | O_Int i => O_Bool (Z.eqb i 0)
-    | O_Bool b => O_Error
-    | O_Error => O_Error
+    | DR_Int i => DR_Bool (Z.eqb i 0)
+    | DR_Bool b => DR_Error
+    | DR_Error => DR_Error
     end
 | (If e1 Then e2 Else e3) => match (evalDupe e1) with 
-    | O_Int i => O_Error
-    | O_Bool b => if b then evalDupe e2  else evalDupe e3 
-    | O_Error => O_Error
+    | DR_Int i => evalDupe e2
+    | DR_Bool b => if b then evalDupe e2  else evalDupe e3 
+    | DR_Error => DR_Error
     end
 end.
 
@@ -84,30 +89,30 @@ where "t 'd-->' t' " := (dupeStep t t').
 (* this will probably have to be changed because of how integers are represented in binary*)
 Reserved Notation " t 'd==>' n " (at level 50, left associativity).
 Inductive dupeEval: dupe -> observable -> Prop :=
-| E_D_Interger: forall z, (D_Integer z) d==> (O_Int z)
-| E_D_Boolean: forall b, (D_Boolean b) d==> (O_Bool b)
+| E_D_Interger: forall z, (D_Integer z) d==> (DR_Int z)
+| E_D_Boolean: forall b, (D_Boolean b) d==> (DR_Bool b)
 
 | E_D_add1: forall t z, 
-    t d==> (O_Int z) ->
-    (D_add1 t) d==> (O_Int (z + 1))
+    t d==> (DR_Int z) ->
+    (D_add1 t) d==> (DR_Int (z + 1))
 | E_D_sub1: forall t z, 
-    t d==> (O_Int z) ->
-    (D_sub1 t) d==> (O_Int (z - 1))
+    t d==> (DR_Int z) ->
+    (D_sub1 t) d==> (DR_Int (z - 1))
 
 | E_D_isZero: forall t z,
-    t d==> (O_Int z) ->
-    (D_zero t) d==> (O_Bool (Z.eqb z 0))
+    t d==> (DR_Int z) ->
+    (D_zero t) d==> (DR_Bool (Z.eqb z 0))
 
 | E_D_ifFalse: forall t_if t_then t_else v,
-    t_if d==> (O_Bool false) -> 
+    t_if d==> (DR_Bool false) -> 
     t_else d==> v ->
     (D_if t_if t_then t_else) d==> v
 | E_D_ifTrue: forall t_if t_then v t_else ,
-    t_if d==> (O_Bool true) -> 
+    t_if d==> (DR_Bool true) -> 
     t_then d==> v ->
     (D_if t_if t_then t_else) d==> v
 | E_D_ifInt: forall t_if z t_then v t_else ,
-    t_if d==> (O_Int z) -> 
+    t_if d==> (DR_Int z) -> 
     t_then d==> v ->
     (D_if t_if t_then t_else) d==> v
 
