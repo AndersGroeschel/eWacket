0aff41203af1e09c2b2567ef362f365efe3cc9f8
Anders Groeschel <andersgroeschel@gmail.com>
2022-12-04 15:45:48 -0500

    renamed typeDefs to utils and added multi stuff from plf

---
+++ src/utils.v
@@ -0,0 +1,39 @@
+Require Import ZArith.
+
+(* this should probably be changed in the future*)
+
+Definition i32 := Z.
+Definition i64 := Z.
+
+
+(* taken from PLF Smallstep.v*)
+Definition relation (X : Type) := X -> X -> Prop.
+
+Inductive multi {X : Type} (R : relation X) : relation X :=
+  | multi_refl : forall (x : X), multi R x x
+  | multi_step : forall (x y z : X),
+                    R x y ->
+                    multi R y z ->
+                    multi R x z.
+
+Theorem multi_R : forall (X : Type) (R : relation X) (x y : X),
+    R x y -> (multi R) x y.
+Proof.
+  intros X R x y H.
+  apply multi_step with y. apply H. apply multi_refl.
+Qed.
+
+
+Theorem multi_trans :
+  forall (X : Type) (R : relation X) (x y z : X),
+      multi R x y  ->
+      multi R y z ->
+      multi R x z.
+Proof.
+  intros X R x y z G H.
+  induction G.
+    - (* multi_refl *) assumption.
+    - (* multi_step *)
+      apply multi_step with y. assumption.
+      apply IHG. assumption.
+Qed.
