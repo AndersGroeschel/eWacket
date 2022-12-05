83d41032766cb504ef3ca48efa03e8a90ddf9985
Anders Groeschel <andersgroeschel@gmail.com>
2022-12-04 22:53:19 -0500

    reverted compiler to use ++ instead of :: because it makes the proof easier

--- src/compiler.v
+++ src/compiler.v
@@ -1,33 +1,24 @@
 Require Import wacket.
-Require Import observable.
 Require Import wasmLite.
 Require Import List.
 
-Require Import String.
 Require Import ZArith.
 Local Open Scope Z_scope.
+Open Scope list_scope.
 
-
-
-Inductive compilerResult : Type :=
-| Succ : wasmCode -> compilerResult
-| Error : string -> compilerResult
-.
 
 (* a compiler from dupe to wasm, does not do any sort of type checking
      and can produce wasm code that can't transition *)
-Fixpoint compile_unsafe (source : dupe) (compiled : wasmCode) := match source with 
-| D_Integer z => ((i64.const z)::compiled)
-| D_Boolean b => ((i32.const (if b then 1 else 0))::compiled)
-| D_add1 e => compile_unsafe e ((i64.const 1)::(i64.add)::compiled)
-| D_sub1 e => compile_unsafe e ((i64.const 1)::(i64.add)::compiled)
-| D_zero e => compile_unsafe e (i64.eqz::compiled)
+Fixpoint compile_unsafe (source : dupe) := match source with 
+| D_Integer z => ((i64.const z)::nil)
+| D_Boolean b => ((i32.const (if b then 1 else 0))::nil)
+| D_add1 e => (compile_unsafe e) ++ ((i64.const 1)::(i64.add)::nil)
+| D_sub1 e => (compile_unsafe e) ++ ((i64.const 1)::(i64.add)::nil)
+| D_zero e => (compile_unsafe e) ++ (i64.eqz::nil)
 | D_if b t e => 
-    (compile_unsafe b 
-    ((ifThenElse 
-        (compile_unsafe t nil) 
-        (compile_unsafe e nil)
-    )::compiled))
+    (compile_unsafe b) ++ 
+    ((ifThenElse (compile_unsafe t) (compile_unsafe e))::nil)
 end.
+
 
 
