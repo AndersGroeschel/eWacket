f355f6366c9a7d33918db401657780ac9938a054
Anders Groeschel <andersgroeschel@gmail.com>
2022-12-08 19:43:16 -0500

    moved some more general tactics to thier own file

--- src/ewacket.v
+++ src/ewacket.v
@@ -5,6 +5,7 @@ Require Import wacket.
 Require Import wasmLite.
 Require Import compiler.
 Require Import utils.
+Require Import helperTactics.
 
 
 
@@ -19,45 +20,10 @@ Ltac applyInstructionOrder :=
 eapply instruction_order;
 eauto.
 
-(* does not work *)
-Ltac refineInductiveHypothesis := 
-match goal with 
-|   H0 : compile ?source = Succ ?compiled,
-    H1 : ?source d==> ?value,
-    IH : forall (c: wasmCode) (d: dupeResult),
-        compile ?source = Succ c ->
-        ?source d==> d -> _
-    |- _ => specialize ((IH ?compiler ?value) H0)
-| _ => fail
-end.
-
 Ltac unfoldCompiled H :=
     unfold compile in H; 
     simpl in H;
     inversion H. 
-
-Ltac logicAuto :=
-match goal with 
-| H: _ /\ _ |- _ => destruct H
-| H: _ \/ _ |- _ => destruct H
-| H: exists _, _ |- _ => destruct H
-| _ => fail
-end.
-
-Ltac destructBools := match goal with 
-| [H : ?x = true |- context[if ?x then _ else _] ] => rewrite H
-| [H : ?x = false |- context[if ?x then _ else _] ] => rewrite H
-| [H : true = ?x |- context[if ?x then _ else _] ] => rewrite H
-| [H : false = ?x |- context[if ?x then _ else _] ] => rewrite H
-| [|- context[if ?x then _ else _] ] => destruct x eqn: eq
-end.
-
-Ltac removeListNils := match goal with
-| [|- context[_ ++ nil] ] => rewrite app_nil_r
-| [|- context[nil ++ _] ] => rewrite app_nil_l
-| [H: context[_ ++ nil] |- _ ] => rewrite app_nil_r  in H
-| [H: context[nil ++ _] |- _ ] => rewrite app_nil_l  in H
-end.
 
 Ltac stepCompletes := (
     reflexivity || 
@@ -78,6 +44,7 @@ Ltac doesSingleStep :=
     || (eapply W_ST_unreachable; stepCompletes)
     )
 .
+
 Ltac doesStep :=
 simpl;
 repeat (
@@ -90,6 +57,17 @@ repeat (
     | [|- _ w--> _] => doesSingleStep
     end
 ).
+
+
+Ltac solveCase := 
+    repeat logicAuto;
+    subst;
+    (repeat refineInductiveHypothesis);
+    (try applyInstructionOrder);
+    (try doesStep);
+    (try assumption)
+.
+
 
 
 (* going to need that dupe is well typed, 
@@ -105,49 +83,30 @@ Proof.
     inversion H0;subst.
     (* int *)
     - unfoldCompiled H.
-        doesStep.
+        solveCase.
     (* bool *)
     - unfoldCompiled H.
-        doesStep.
+        solveCase.
     (* add 1*)
-    - apply add1_ImpliesSource in H. 
-        repeat logicAuto.
-        subst.
-        applyInstructionOrder.
-        doesStep.
+    - apply add1_ImpliesSource in H.
+        solveCase.
     (* sub1 *)
     - apply sub1_ImpliesSource in H. 
-        repeat logicAuto.
-        subst.
-        applyInstructionOrder.
-        doesStep.
+        solveCase.
     (* eqz *)
     - apply zero_ImpliesSource in H. 
-        repeat logicAuto.
-        subst.
-        applyInstructionOrder.
-        doesStep.
+        solveCase.
     (* if false*)
-    - apply (ifBool_ImpliesSource source1 source2 source3 compiled #f) in H. 
-        repeat logicAuto.
-        subst.
-        + apply (IHsource3 x1 dupeResult) in H6.
-            * applyInstructionOrder. doesStep.
-            * assumption.
+    - apply (ifBool_ImpliesSource source1 source2 source3 compiled #f) in H.
+        + solveCase.
         + assumption.
     (* if true *)
     - apply (ifBool_ImpliesSource source1 source2 source3 compiled #t) in H. 
-        repeat logicAuto.
-        subst.
-        + apply (IHsource2 x0 dupeResult) in H6.
-            * applyInstructionOrder. doesStep.
-            * assumption.
+        + solveCase.
         + assumption.
     (* if int *)
     - apply (ifInt_ImpliesSource source1 source2 source3 compiled z) in H. 
-        repeat logicAuto.
-        subst.
-        + apply (IHsource2 x0 dupeResult) in H6; assumption.
+        + solveCase.
         + assumption.
 Qed.
 
